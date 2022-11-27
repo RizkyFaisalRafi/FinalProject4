1a25bb494bc5c6073bb350873fe4d82e6d5e3a55
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2022-11-27 18:49:18 +0700

    Delete

--- app/src/main/java/com/rifara/travelling/MainActivity.java
+++ app/src/main/java/com/rifara/travelling/MainActivity.java
@@ -18,52 +18,11 @@ import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
-    GoogleSignInOptions googleSignInOptions;
-    GoogleSignInClient googleSignInClient;
-    TextView name, email;
-    Button signOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = findViewById(R.id.tv_name);
-        email = findViewById(R.id.tv_email);
-        signOutBtn = findViewById(R.id.btn_sign_out);
-
-        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
-
-
-
-        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
-        if (googleSignInAccount != null) {
-            String personName = googleSignInAccount.getDisplayName();
-            String personEmail = googleSignInAccount.getEmail();
-
-            name.setText(personName);
-            email.setText(personEmail);
-        }
-
-        signOutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signOut();
-            }
-        });
-
     }
-
-
-    void signOut() {
-        googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(Task<Void> task) {
-                finish();
-                startActivity(new Intent(MainActivity.this, SignInActivity.class));
-            }
-        });
-    }
-
 }
