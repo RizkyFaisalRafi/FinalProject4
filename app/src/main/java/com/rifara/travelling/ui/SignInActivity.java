31c911e429939b3a26b92fbfa816ff3ce34e91b3
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2022-12-15 21:06:37 +0700

    Fixs Login Google

--- app/src/main/java/com/rifara/travelling/ui/SignInActivity.java
+++ app/src/main/java/com/rifara/travelling/ui/SignInActivity.java
@@ -2,6 +2,7 @@ package com.rifara.travelling.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
@@ -36,6 +37,8 @@ public class SignInActivity extends AppCompatActivity {
 
     GoogleSignInOptions googleSignInOptions;
     GoogleSignInClient googleSignInClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,17 +57,24 @@ public class SignInActivity extends AppCompatActivity {
 
         // Google
         googlebtn = findViewById(R.id.linear_button_login_google);
+
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
         googlebtn.setOnClickListener((v) -> loginGoogle());
 
     }
 
     // Google
     private void loginGoogle() {
-        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+//        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+//        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
 
         Intent signInIntent = googleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, 1000);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     // Google
@@ -72,20 +82,52 @@ public class SignInActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1000) {
+//        if (requestCode == RC_SIGN_IN) {
+//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+//
+//            try {
+//                task.getResult(ApiException.class);
+//                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+//                finish();
+//
+//            } catch (ApiException e) {
+//                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//
+//        } else {
+//            Utility.showToast(SignInActivity.this, "Gagal");
+//        }
+
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
 
-            try {
-                task.getResult(ApiException.class);
-                startActivity(new Intent(SignInActivity.this, MainActivity.class));
-                finish();
+    }
 
-            } catch (ApiException e) {
-                Toast.makeText(getApplicationContext(), "Something Went Wrong!", Toast.LENGTH_SHORT).show();
-            }
 
-        } else {
-            Utility.showToast(SignInActivity.this, "Gagal");
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            Log.d(TAG, "Success Signin Google:" + account.getId() +" - "+account.getDisplayName());
+            Log.d(TAG, "Id:" + account.getId());
+            Log.d(TAG, "Id Token:" + account.getIdToken());
+            Log.d(TAG, "Display Name:" +account.getDisplayName());
+
+            // Signed in successfully, show authenticated UI.
+            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+            finish();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(getApplicationContext(), "Something Went Wrong! "+"signInResult:failed code=" + e.getStatusCode(), Toast.LENGTH_SHORT).show();
         }
     }
 
